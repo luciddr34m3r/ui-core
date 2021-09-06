ce94788925bb43c70685c4fd9cf4fb89ecfdcc51
Curt Barnard <luciddr34m3r@gmail.com>
2021-09-06 04:40:47 +0000

    refactor

--- ui-core/src/components/index.js
+++
@@ -1,9 +0,0 @@
-import ActionList from "./DataDisplay/ActionList";
-import PaginatedDataTable from "./DataDisplay/PaginatedDataTable";
-import MainNavigation from "./Navigation/MainNavigation";
-
-export {
-  ActionList,
-  PaginatedDataTable,
-  MainNavigation,
-};
--- ui-core/src/test/DataDisplay/ActionList.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import ActionList from '../../components/DataDisplay/ActionList';
-
-describe('<ActionList />', () => {
-  it('should render', () => {
-    render(<ActionList />);
-  })
-});
